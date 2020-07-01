f0b4d4cc0f5e2f52f9be08c6e662caa1a8a7108c
MateuszMichalik93 <59762524+MateuszMichalik93@users.noreply.github.com>
2020-07-01 12:24:40 +0200

    dodanie rozwiazan do zadania2

--- src/zadanko2/ZadaniaTablice.java
+++ src/zadanko2/ZadaniaTablice.java
@@ -11,8 +11,44 @@ public class ZadaniaTablice {
         System.out.println(sum);
     }
 
+    public void avgTab(){
+        int[] tablica = {1, 2, 3, 4, 5};
+        double avgTab = 0;
+
+        avgTab = sum / tablica.length;
+
+        System.out.println(avgTab);
+        }
+
+        public void maxLiczba(){
+            int[] tablica = {1, 2, 3, 4, 5};
+            int maxLiczba = tablica[0];
+
+             for (int i = 0; i < tablica.length; i++){
+             if(tablica[i] > maxLiczba){
+                 maxLiczba = tablica[i];
+             }
+             }
+            System.out.println(maxLiczba);
+        }
+
+        public void minLiczba(){
+            int[] tablica = {1, 2, 3, 4, 5};
+            int minLiczba = tablica[0];
+            for (int i = 0; i < tablica.length; i++){
+                if(minLiczba > tablica[i]){
+                    minLiczba = tablica[i];
+                }
+            }
+            System.out.println(minLiczba);
+
+        }
+
     public static void main(String[] args) {
         ZadaniaTablice zadaniaTablice = new ZadaniaTablice();
         zadaniaTablice.sumTab();
+       zadaniaTablice.avgTab();
+       zadaniaTablice.maxLiczba();
+       zadaniaTablice.minLiczba();
     }
 }
